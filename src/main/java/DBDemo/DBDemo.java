a3849e10c97b28b366c5923d2eacb597de8ab285
ganirockz <ganeshrapeti8@gmail.com>
2020-10-29 21:07:19 +0530

    [Ganesh]Added: Create payroll_service database and java program to connect to it

---
+++ src/main/java/DBDemo/DBDemo.java
@@ -0,0 +1,42 @@
+package DBDemo;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.time.LocalDate;
+import java.util.Enumeration;
+
+public class DBDemo {
+	public static void main(String[] args) {
+		String jdbcURL = "jdbc:mysql://localhost:3306/payroll_service?useSSL=false";
+		String userName = "root";
+		String password = "@GaniMySql02";
+		Connection connection = null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			System.out.println("Driver loaded");
+		} catch (ClassNotFoundException e) {
+			throw new IllegalStateException("Cannot find the driver in the class");
+		}
+		listDrivers();
+		try {
+			System.out.println("Connecting to database:" + jdbcURL);
+			connection = DriverManager.getConnection(jdbcURL, userName, password);
+			System.out.println("Connection is successfull!!!" + connection);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static void listDrivers() {
+		Enumeration<Driver> driverList = DriverManager.getDrivers();
+		while (driverList.hasMoreElements()) {
+			Driver driverClass = (Driver) driverList.nextElement();
+			System.out.println("   " + driverClass.getClass().getName());
+		}
+	}
+}
