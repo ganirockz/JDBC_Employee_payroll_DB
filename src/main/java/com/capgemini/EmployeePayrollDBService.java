cbcb68e78d353a7334574a7181598e29ade99c94
ganirockz <ganeshrapeti8@gmail.com>
2020-11-02 12:46:22 +0530

    [Ganesh]Added: add new employee to db after creating ER model

--- src/main/java/com/capgemini/EmployeePayrollDBService.java
+++ src/main/java/com/capgemini/EmployeePayrollDBService.java
@@ -257,4 +257,90 @@ public class EmployeePayrollDBService {
 		}
 		return null;
 	}
+
+	public EmployeePayrollData addEmployeeToPayrollERDiagram(String name, double salary, LocalDate startDate,
+			String gender) {
+		int empId = -1;
+		EmployeePayrollData employeePayrollData = null;
+		Connection connection = null;
+		try {
+			connection = this.getConnection();
+			connection.setAutoCommit(false);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		try (Statement statement = connection.createStatement()) {
+			String sql = String.format(
+					"insert into employee_payroll (name,basic_pay,start,gender) values ('%s',%.2f,'%s','%s');", name,
+					salary, startDate.toString(), gender);
+			int rowsAffected = statement.executeUpdate(sql, statement.RETURN_GENERATED_KEYS);
+			if (rowsAffected == 1) {
+				ResultSet resultSet = statement.getGeneratedKeys();
+				if (resultSet.next())
+					empId = resultSet.getInt(1);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			try {
+				connection.rollback();
+				return employeePayrollData;
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
+		}
+		int employeeId = 0;
+		try (Statement statement = connection.createStatement()) {
+			String sql = String.format("insert into employee (name,gender) values ('%s','%s');", name, gender);
+			int rowsAffected = statement.executeUpdate(sql, statement.RETURN_GENERATED_KEYS);
+			if (rowsAffected == 1) {
+				ResultSet resultSet = statement.getGeneratedKeys();
+				if (resultSet.next())
+					employeeId = resultSet.getInt(1);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			try {
+				connection.rollback();
+				return employeePayrollData;
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
+		}
+		try (Statement statement = connection.createStatement()) {
+			double deductions = salary * 0.2;
+			double taxablePay = salary - deductions;
+			double tax = taxablePay * 0.1;
+			double netPay = salary - tax;
+			String sql = String.format(
+					"insert into payroll (id,basic_pay,deductions,taxable_pay,tax,net_pay) values (%s,%s,%s,%s,%s,%s);",
+					employeeId, salary, deductions, taxablePay, tax, netPay);
+			int rowsAffected = statement.executeUpdate(sql, statement.RETURN_GENERATED_KEYS);
+			if (rowsAffected == 1) {
+				employeePayrollData = new EmployeePayrollData(empId, name, salary, startDate);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			try {
+				connection.rollback();
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
+		}
+		try {
+			try {
+				connection.commit();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		} finally {
+			if (connection != null) {
+				try {
+					connection.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return employeePayrollData;
+	}
 }
